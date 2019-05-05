c14d46a4f8a6fecd5e3edd13d1e880426a69f481
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-05 14:53:33 +0000

    Deploy fccleaderboard.mikelowe.xyz to github.com/vanillaSlice/FCCLeaderboard.git:gh-pages

---
+++ precache-manifest.6cc7c84bdb4f1a10e117841732f1f64a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ca60b6de91ee9c5f306",
+    "url": "/static/css/main.94cbd80d.chunk.css"
+  },
+  {
+    "revision": "9ca60b6de91ee9c5f306",
+    "url": "/static/js/main.9ca60b6d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2105b08948c5c9a0fef1",
+    "url": "/static/css/2.6ba8a23e.chunk.css"
+  },
+  {
+    "revision": "2105b08948c5c9a0fef1",
+    "url": "/static/js/2.2105b089.chunk.js"
+  },
+  {
+    "revision": "d1c23cc90df19e23e691616f34e6c99c",
+    "url": "/static/media/freecodecamp_logo.d1c23cc9.svg"
+  },
+  {
+    "revision": "557340b9d5127938572b2c620544ae7f",
+    "url": "/index.html"
+  }
+];
